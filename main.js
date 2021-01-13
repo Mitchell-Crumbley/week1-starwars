3080fb8a246f1327bde918079fd0a5d96265b7aa
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-12 21:15:30 -0600

    Final updates to Star Wars Page

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Mitchell Crumbley");
+
+$('.hover').mouseover(function() {
+  $('.text').css("visibility","visible");
+});
+
+$('.hover').mouseout(function() {
+  $('.text').css("visibility","hidden");
+});
